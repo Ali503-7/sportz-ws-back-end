0073312acfc7257ec27a5a02ff80d338ece77262
Ali Mohamed <alimoali354@gmail.com>
2026-08-19 20:20:54 +0300

    feat: add post matches

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import express, { Request, Response } from "express";
+import { matchRouter } from "./routes/match";
 
 const app = express();
 const PORT = 8000;
 
+app.use(express.json());
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello from Express TS!");
 });
+
+app.use("/matches", matchRouter);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port http://localhost:${PORT}`);
